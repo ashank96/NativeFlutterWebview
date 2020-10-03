fc9d4159bdc01b48a749bdb77d62f5efd2f724b5
Ashank Bharati <bharatiashank1996@gmail.com>
2020-10-03 17:58:22 +0530

    add comment for JS interface

--- app/src/main/java/com/ashank/nativeflutterwebview/FlutterWebViewInterface.java
+++ app/src/main/java/com/ashank/nativeflutterwebview/FlutterWebViewInterface.java
@@ -12,9 +12,9 @@ public class FlutterWebViewInterface {
     public FlutterWebViewInterface(Context c) {
         context = c;
     }
-
+    //this method is called by flutter web before initialisation
     @JavascriptInterface
     public String getString() {
-        return "Hello from Native"; // FlutterCoreMethodHandler.getHostName();
+        return "Hello from Native"; 
     }
 }
